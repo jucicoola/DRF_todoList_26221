a054b7ee013d55785a4007def26aebb16babdd69
Jongha Mun <jucicoola@gmail.com>
2026-03-11 18:19:38 +0900

    feat: postgresql 전환

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -100,12 +100,24 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mysite_db",
+        "USER": "mysite_user",
+        "PASSWORD": "mysite_password",
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
+
 
 
 # Password validation
